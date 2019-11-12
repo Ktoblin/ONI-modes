e8fdb20914549f19d30ff1e3d0dc54950cd4f16a
Ktoblin <mr.ktoblin@gmail.com>
2019-11-12 20:27:36 +0300

    Slime refinery added

--- BleachStoneRefinery/src/BleachStoneRefinery.cs
+++ BleachStoneRefinery/src/BleachStoneRefinery.cs
@@ -40,37 +40,6 @@ namespace BleachStoneRefinery
         }
 
     }
-
-    //ColorPatch
-    [HarmonyPatch(typeof(BuildingComplete), "OnSpawn")]
-    public class ColorPatch
-    {
-        public static void Postfix(BuildingComplete __instance)
-        {
-            var kAnimBase = __instance.GetComponent<KAnimControllerBase>();
-            if (kAnimBase != null)
-            {
-                Debug.Log(__instance.name);
-                if (__instance.name == "BleachStoneRefineryComplete")
-                {
-                    float r = 255 - 60;
-                    float g = 255 - 50;
-                    float b = 255 - 130;
-                    kAnimBase.TintColour = new Color(r / 255f, g / 255f, b / 255f);
-                }
-            }
-        }
-    }
-
-    [HarmonyPatch(typeof(Db), "Initialize")]
-    public class DbPatch
-    {
-        public static void Prefix()
-        {
-            List<string> ls = new List<string>(Techs.TECH_GROUPING["Catalytics"]) { BleachStoneRefineryConfig.ID };
-            Techs.TECH_GROUPING["Catalytics"] = ls.ToArray();
-        }
-    }
 }
 
 
---
+++ BleachStoneRefinery/src/Patch.cs
@@ -0,0 +1,47 @@
+﻿using Database;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BleachStoneRefinery
+{
+    [HarmonyPatch(typeof(BuildingComplete), "OnSpawn")]
+    public class ColorPatch
+    {
+        public static void Postfix(BuildingComplete __instance)
+        {
+            var kAnimBase = __instance.GetComponent<KAnimControllerBase>();
+            if (kAnimBase != null)
+            {
+                Debug.Log(__instance.name);
+                if (__instance.name == "BleachStoneRefineryComplete")
+                {
+                    float r = 255 - 60;
+                    float g = 255 - 50;
+                    float b = 255 - 130;
+                    kAnimBase.TintColour = new Color(r / 255f, g / 255f, b / 255f);
+                }
+                if (__instance.name == "SlimeRefineryComplete")
+                {
+                    float r = 255 - 127;
+                    float g = 255 - 159;
+                    float b = 255 - 159;
+                    kAnimBase.TintColour = new Color(r / 255f, g / 255f, b / 255f);
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Db), "Initialize")]
+    public class DbPatch
+    {
+        public static void Prefix()
+        {
+            List<string> ls = new List<string>(Techs.TECH_GROUPING["Catalytics"]) { BleachStoneRefineryConfig.ID, SlimeRefineryConfig.ID };
+            Techs.TECH_GROUPING["Catalytics"] = ls.ToArray();
+        }
+    }
+}
